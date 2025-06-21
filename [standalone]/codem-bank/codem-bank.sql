c80e25a6a08eafd0114b46912b406db366747ecb
_-Lukas-_ <47757792+IstMirWayne@users.noreply.github.com>
2025-06-21 20:46:44 +0200

    banking

---
+++ [standalone]/codem-bank/codem-bank.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS `codem_newbank` (
+  `identifier` char(50) DEFAULT NULL,
+  `profiledata` longtext DEFAULT NULL,
+  `spenddata` longtext DEFAULT NULL,
+  `cryptodata` longtext DEFAULT NULL,
+  `loandata` longtext DEFAULT NULL,
+  `savingdata` longtext DEFAULT NULL,
+  `notificationdata` longtext DEFAULT NULL,
+  `mostsenddata` longtext DEFAULT NULL,
+  `spendigsdata` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+CREATE TABLE IF NOT EXISTS `codem_newbank_society` (
+  `jobname` char(50) DEFAULT NULL,
+  `playersdata` longtext DEFAULT NULL,
+  `spenddata` longtext DEFAULT NULL,
+  `notificationdata` longtext DEFAULT NULL,
+  `contributiondata` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+ALTER TABLE `players` ADD `iban` char(50) DEFAULT NULL;
+
+ALTER TABLE `users` ADD `iban` char(50) DEFAULT NULL;
